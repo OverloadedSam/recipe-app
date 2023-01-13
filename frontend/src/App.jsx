53255ecbed02c9da946d1c33c54d0659dc154205
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-13 21:35:17 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Typography from '@mui/material/Typography';
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Typography variant='h1'>Recipe App</Typography>
+    </div>
+  );
+};
+
+export default App;
