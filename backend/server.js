50e2bea5d064db456df988ddad71fe96ec2473a2
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-13 22:05:25 +0530

    Define error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const errorHandler = require('./middlewares/errorHandler');
 const userRoutes = require('./routes/users');
 const recipeRoutes = require('./routes/recipes');
 
@@ -19,6 +20,9 @@ connectDB();
 const baseUrl = process.env.BASE_API_URL;
 server.use(baseUrl, userRoutes);
 server.use(baseUrl, recipeRoutes);
+
+/* CUSTOM ERROR HANDLER */
+server.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
