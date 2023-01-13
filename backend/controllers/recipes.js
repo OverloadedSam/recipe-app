617d9c89ede86e5fa2670686b1263d680d1056eb
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-14 01:08:06 +0530

    Add route and its controller to get a recipe details

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -21,3 +21,32 @@ module.exports.getRecipes = asyncHandler(async (req, res, next) => {
     data: recipes,
   });
 });
+
+/*
+ * @ROUTE    GET /api/recipe/:id
+ * @DESC     Get recipe full details with steps/process.
+ * @ACCESS   Protect
+ * */
+module.exports.getRecipeById = asyncHandler(async (req, res, next) => {
+  const recipeId = req.params.id;
+
+  const recipe = await Recipe.findById(recipeId).select('-__v -updatedAt');
+  if (!recipe) {
+    const message = `Recipe not found with given id: ${recipeId}`;
+    return next(new ErrorResponse(404, message));
+  }
+
+  const ingredients = await Ingredient.find({ recipe: recipeId }).select(
+    '-recipe -__v -createdAt -updatedAt'
+  );
+
+  const process = await Process.find({ recipe: recipeId })
+    .select('-recipe -__v -createdAt -updatedAt')
+    .sort({ createdAt: 'asc' });
+
+  res.json({
+    success: true,
+    status: 200,
+    data: { ...recipe._doc, ingredients, process },
+  });
+});
--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const validateObjectId = require('../middlewares/validateObjectId');
 const auth = require('../middlewares/auth');
-const { getRecipes } = require('../controllers/recipes');
+const { getRecipes, getRecipeById } = require('../controllers/recipes');
 
 router.get('/recipes', auth, getRecipes);
+router.get('/recipe/:id', validateObjectId('id'), auth, getRecipeById);
 
 module.exports = router;
