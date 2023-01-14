c0e2d351f39f24765d7c83212aa90d014235c659
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-14 15:54:35 +0530

    Create material ui custom theme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
+import { useMemo } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
+import { themeOptions } from './theme';
 
 const App = () => {
+  const theme = useMemo(() => createTheme(themeOptions), [themeOptions]);
+
   return (
     <div className='app'>
-      <Typography variant='h1'>Recipe App</Typography>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Typography variant='h2'>Recipe App</Typography>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
---
+++ frontend/src/theme.js
@@ -0,0 +1,36 @@
+export const themeOptions = {
+  palette: {
+    type: 'light',
+    primary: {
+      100: '#fdccdd',
+      200: '#fb99bb',
+      300: '#f9669a',
+      400: '#f73378',
+      500: '#f50056',
+      600: '#c40045',
+      700: '#930034',
+      800: '#620022',
+      900: '#310011',
+      main: '#f50056',
+      light: '#f73377',
+      dark: '#ab003c',
+    },
+    secondary: {
+      100: '#d1dce6',
+      200: '#a2b8cd',
+      300: '#7495b3',
+      400: '#45719a',
+      500: '#174e81',
+      600: '#123e67',
+      700: '#0e2f4d',
+      800: '#091f34',
+      900: '#05101a',
+      main: '#174e81',
+      light: '#45719a',
+      dark: '#10365a',
+    },
+  },
+  typography: {
+    fontFamily: ['Open Sans', 'sans-serif'].join(','),
+  },
+};
